3a4e309eae189b5ebbd278f36122e3c389de6f2f
Angelina Gridasova <gridasovaangelina@mail.ru>
2025-10-15 16:51:54 +0300

    two classes are written

--- classes.py
+++ classes.py
@@ -1,13 +1,38 @@
 #тут будут все классы
 #книга (Book)
+
 #автор (Author)
+class Author:
+    def __init__(self, first_name: str, last_name: str, bio: str = ""):
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
 #место книги (Location)
+class Location:
+    def __init__(self, rack: str, shelf: str):
+        self.rack: str = rack
+        self.shelf: str = shelf
+
+    def __str__(self) -> str:
+        return f"стеллаж {self.rack}, полка {self.shelf}"
+    
 #читатель (Reader)
+
 #библиотекарь (Librarian)
+
 #школьник (School)
+
 #студент (Student)
+
 #читательный зал (Room)
+
 #читательский билет (Ticket)
+
 #отзыв (Review) 
+
 #читательский клуб (Club)
+
  
